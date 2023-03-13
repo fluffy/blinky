17e45b1a10979f649ccbedaef021c780d24177a6
Cullen Jennings <fluffy@iii.ca>
2023-03-13 07:13:40 -0600

    add generated STM32 code

---
+++ Core/Inc/main.h
@@ -0,0 +1,103 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LEDM3_Pin GPIO_PIN_0
+#define LEDM3_GPIO_Port GPIOA
+#define LEDM1_Pin GPIO_PIN_1
+#define LEDM1_GPIO_Port GPIOA
+#define LEDM2_Pin GPIO_PIN_2
+#define LEDM2_GPIO_Port GPIOA
+#define COL2_Pin GPIO_PIN_7
+#define COL2_GPIO_Port GPIOA
+#define COL5_Pin GPIO_PIN_0
+#define COL5_GPIO_Port GPIOB
+#define COL4_Pin GPIO_PIN_1
+#define COL4_GPIO_Port GPIOB
+#define COL3_Pin GPIO_PIN_2
+#define COL3_GPIO_Port GPIOB
+#define COL2B10_Pin GPIO_PIN_10
+#define COL2B10_GPIO_Port GPIOB
+#define COL1_Pin GPIO_PIN_11
+#define COL1_GPIO_Port GPIOB
+#define ROW4_Pin GPIO_PIN_6
+#define ROW4_GPIO_Port GPIOC
+#define ROW3_Pin GPIO_PIN_7
+#define ROW3_GPIO_Port GPIOC
+#define ROW2_Pin GPIO_PIN_8
+#define ROW2_GPIO_Port GPIOC
+#define ROW1_Pin GPIO_PIN_9
+#define ROW1_GPIO_Port GPIOC
+#define LEDE_Pin GPIO_PIN_9
+#define LEDE_GPIO_Port GPIOA
+#define LEDD_Pin GPIO_PIN_10
+#define LEDD_GPIO_Port GPIOA
+#define LEDC_Pin GPIO_PIN_11
+#define LEDC_GPIO_Port GPIOA
+#define LEDB_Pin GPIO_PIN_12
+#define LEDB_GPIO_Port GPIOA
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
