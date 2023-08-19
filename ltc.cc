6371437f2ed91a8b3268020bb821946f59bc0700
Cullen Jennings <fluffy@iii.ca>
2023-08-19 16:31:58 -0600

    start decode

--- ltc.cc
+++ ltc.cc
@@ -4,11 +4,13 @@
 
 class TimeCode {
  public:
-  TimeCode(uint32_t s, uint32_t us ) { frame=(us*30l/1000000l)%30; sec=s%60; min=(s/60)%60; hour=(s/3600)%24;  };
+  TimeCode() { valid=0; };
+  TimeCode(uint32_t s, uint32_t us ) { frame=(us*30l/1000000l)%30; sec=s%60; min=(s/60)%60; hour=(s/3600)%24; valid=1; };
   
   TimeCode(uint8_t h, uint8_t m, uint8_t s, uint8_t f)
       : hour(h), min(m), sec(s), frame(f){};
   uint32_t disp() {
+    if ( !valid ) { return 0; };
     return frame + sec * 100l + min * 10000l + hour * 1000000l;
   }
 
@@ -17,17 +19,35 @@ class TimeCode {
   uint8_t min;
   uint8_t sec;
   uint8_t frame;
+  uint8_t valid;
 };
+
+
+class TransitionSet {
+public:
+  TransitionSet(): size(0) {};
+public:
+  static const int maxTransitions=161;
+  uint32_t t[maxTransitions];
+  uint8_t size; 
+};
+
 
 class LTC {
  public:
-  LTC( const TimeCode& time) { set(time); };
+  LTC() { valid=0; };
+  LTC( const TimeCode& time) { set(time); valid=1; };
   
   void set(const TimeCode& time);
-
+  void get( TimeCode& time);
+  
+  void encode( TransitionSet& tSet );
+  void decode( const TransitionSet& tSet );
+  
  public:
   uint8_t bits[10];
   uint8_t parity();
+  uint8_t valid;
 };
 
 uint8_t LTC::parity(){
@@ -46,43 +66,89 @@ uint8_t LTC::parity(){
   return ret%2;
 }
 
-class TransitionSet {
-public:
-  TransitionSet( const LTC& ltc );
-public:
-  static const int maxTransitions=161;
-  uint16_t t[maxTransitions];
-  uint8_t size; 
-};
-
-TransitionSet::TransitionSet( const LTC& ltc ){
-  size=0;
+void LTC::encode( TransitionSet& tSet ) {
+  // TransitionSet::TransitionSet( const LTC& ltc ){
+  tSet.size=0;
   uint32_t time=0;
   // baud rate is 30 fps, 80 bits per frame , two transitions per bit for 1's = 2400 Hz for 1 
   uint32_t oneInc=1000000/2400; // compute from baud and clk rate. Rate for a 1 (2 tranition)
   uint32_t zeroInc=oneInc*2; // compute from baud and clk rate. Rate for a 1 (2 tranition)
 
-  t[size++] = time;
+  tSet.t[tSet.size++] = time;
   
    for ( int i=0; i<=10 ; i++ ) {
-    uint8_t data = ltc.bits[i];
+    uint8_t data = bits[i];
     for ( int bit=0; bit<=8 ; bit++ ) {
       if ( data & 0x1 ) {
 	// encode a one
 	time += oneInc;
-	t[size++] = time;
+	tSet.t[tSet.size++] = time;
 	time += oneInc;
-	t[size++] = time;
+	tSet.t[tSet.size++] = time;
       } else {
 	// encode a zero
 	time += zeroInc;
-	t[size++] = time;
+	tSet.t[tSet.size++] = time;
       }      
       data = data >> 1;
     }
   }
   
 }
+
+   
+void LTC::decode( const TransitionSet& tSet ){
+  valid = 0;
+
+  uint8_t bitCount=0;
+  uint8_t byteCount=0;
+
+  for ( int i=0; i<10; i++ ) {
+    bits[i]=0;
+  }
+  
+  for ( int i=1; i< tSet.size; i++ ) {
+    uint32_t delta = tSet.t[i] - tSet.t[i-1];
+
+    if (  ( delta > 700 ) && ( delta < 900 ) ) {
+      // found a zero
+      std::cout << "0"  ; 
+      
+      bitCount++;
+      if ( bitCount >= 8 ) { bitCount=0; byteCount++; }
+      if ( byteCount >= 10 ) { return; };
+      
+    } else  if (  ( delta > 300 ) && ( delta < 500 ) ) {
+      // start of 1
+      if ( i+1 >=  tSet.size ) { return; }
+      uint32_t delta2 = tSet.t[i+1] - tSet.t[i-2];
+      if (  ( delta > 300 ) && ( delta < 500 ) ) {
+	// found a 1
+	std::cout << "1"  ;
+ 
+	bits[byteCount] |= ( 1 << bitCount );
+	bitCount++;
+	if ( bitCount >= 8 ) { bitCount=0; byteCount++; }
+	if ( byteCount >= 10 ) { return; }
+    
+	i++; // skip 2nd transtion on  main loop 	
+      } else {
+	return;
+      }
+    }
+    else {
+      // error
+      std::cout << " bad delta=" << delta << std::endl  ; 
+      return;
+    }
+  }
+  
+  if  ( parity() != 0 ) { return; }
+  if  (  bits[8] != 0x3F ) { return; }
+  if  (  bits[9] != 0xFE ) { return; }
+  valid = 1;
+}
+
 
 void LTC::set(const TimeCode& time) {
   bits[0] = time.frame % 10;
@@ -102,20 +168,51 @@ void LTC::set(const TimeCode& time) {
   }
 }
 
+
+void LTC::get( TimeCode& time) {
+  time.valid = 0;
+
+  if ( !valid ) { return; }
+  if ( bits[8] != 0x3F ) { return; }
+  if ( bits[9] != 0xF3 ) { return; }
+  
+  time.frame = ( bits[1] & 0x03 )*10 + (bits[0] & 0x0F);
+  time.sec =   ( bits[3] & 0x07 )*10 + (bits[2] & 0x0F);
+  time.min =   ( bits[5] & 0x07 )*10 + (bits[4] & 0x0F);
+  time.hour =  ( bits[7] & 0x03 )*10 + (bits[6] & 0x0F);
+  
+  time.valid = 1; 
+}
+
+
 int main(int argc, char* argv[]) {
-  std::cout << "starting" << std::endl;
+  std::cout << "LTC Test" << std::endl;
 
   TimeCode t1(11*60+22,500000l);
   std::cout << "t1=" << t1.disp() << std::endl;
 
   LTC ltc1( t1 );
 
-  TransitionSet tSet( ltc1 );
-
+  TransitionSet tSet;
+  ltc1.encode( tSet );
+  
+#if 0 
   std::cout << "tSet size=" << (int)tSet.size << std::endl;
   for( int i=1; i < tSet.size; i++ ){
     std::cout << "t[" << i << "]=" << tSet.t[i] << "   " << tSet.t[i]- tSet.t[i-1] << std::endl;
   }
-  
+#endif
+
+  LTC ltc2;
+  ltc2.decode( tSet );
+
+  std::cout << "done decode" <<  std::endl;
+
+   
+  TimeCode t2;
+  ltc2.get(t2); 
+
+  std::cout << "t2=" << t2.disp() << std::endl;
+
   return 0;
 }
