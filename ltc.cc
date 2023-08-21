76b40ce169e7d4ba85e1e57542c5d50d05af375a
Cullen Jennings <fluffy@iii.ca>
2023-08-21 12:38:29 -0600

    clean up

--- ltc.cc
+++ ltc.cc
@@ -2,7 +2,11 @@
 
 #include <iostream>
 
+class LTC;
+
 class TimeCode {
+  friend class LTC;
+  
  public:
   TimeCode() { valid = 0; };
   TimeCode(uint32_t s, uint32_t us) {
@@ -15,6 +19,10 @@ class TimeCode {
 
   TimeCode(uint8_t h, uint8_t m, uint8_t s, uint8_t f)
       : hour(h), min(m), sec(s), frame(f){};
+
+  uint32_t seconds() { if (!valid) return 0; return ( uint32_t)sec + ( uint32_t) min * 60l + ( uint32_t)hour * 3600l; };
+  uint32_t microSeconds() { if (!valid) return 0; return  ( uint32_t)frame*1000000l/30l; };
+  
   uint32_t disp() {
     if (!valid) {
       return 0;
@@ -22,7 +30,7 @@ class TimeCode {
     return frame + sec * 100l + min * 10000l + hour * 1000000l;
   }
 
- public:
+ private:
   uint8_t hour;
   uint8_t min;
   uint8_t sec;
@@ -31,13 +39,26 @@ class TimeCode {
 };
 
 class TransitionSet {
- public:
-  TransitionSet() : size(0){};
-
- public:
-  static const int maxTransitions = 161;
-  uint32_t t[maxTransitions];
-  uint8_t size;
+  //friend class LTC;
+public:
+  TransitionSet() : numTransitions(0){};
+  void add(  uint32_t t ) {
+    if ( numTransitions >= maxTransitions ) return;
+    time[numTransitions]=t;
+    numTransitions++;
+  } ;
+  uint32_t delta(  uint16_t i ) const
+  {
+    if ( (i<1) || (i>=numTransitions) ) return 0;
+    return time[i] - time[i-1];
+  };
+  uint16_t size() const { return numTransitions; } ;
+  void clear() { numTransitions=0; };
+  
+private:
+  static const uint16_t maxTransitions = 80*2+1;
+  uint32_t time[maxTransitions];
+  uint16_t numTransitions;
 };
 
 class LTC {
@@ -78,7 +99,8 @@ uint8_t LTC::parity() {
 
 void LTC::encode(TransitionSet& tSet) {
   // TransitionSet::TransitionSet( const LTC& ltc ){
-  tSet.size = 0;
+  tSet.clear();
+  
   uint32_t time = 0;
   // baud rate is 30 fps, 80 bits per frame , two transitions per bit for 1's =
   // 2400 Hz for 1
@@ -88,7 +110,7 @@ void LTC::encode(TransitionSet& tSet) {
   uint32_t zeroInc =
       oneInc * 2;  // compute from baud and clk rate. Rate for a 1 (2 tranition)
 
-  tSet.t[tSet.size++] = time;
+  tSet.add( time );
 
   for (int i = 0; i < 10; i++) {
     uint16_t data = bits[i];
@@ -97,15 +119,15 @@ void LTC::encode(TransitionSet& tSet) {
       if ( (data>>bit) & 0x1) {
         // encode a one
         time += oneInc;
-        tSet.t[tSet.size++] = time;
+        tSet.add(time );
         time += oneInc;
-        tSet.t[tSet.size++] = time;
+        tSet.add(time );
 	
 	 std::cout << "1";
       } else {
         // encode a zero
         time += zeroInc;
-        tSet.t[tSet.size++] = time;
+        tSet.add(time );
 
 	 std::cout << "0";
       }
@@ -124,15 +146,15 @@ void LTC::decode(const TransitionSet& tSet) {
     bits[i] = 0;
   }
 
-  for (int i = 1; i < tSet.size; i++) {
+  for (int i = 1; i < tSet.size(); i++) {
       if (byteCount >= 10) {
-        std::cout << " too many bytes i=" << i << " of " << (int)tSet.size
+        std::cout << " too many bytes i=" << i << " of " << (int)tSet.size()
                   << std::endl;
         return;
       }
 
-    uint32_t delta = tSet.t[i] - tSet.t[i - 1];
-
+      uint32_t delta = tSet.delta(i);
+      
     if ((delta > 700) && (delta < 900)) {
       // found a zero
       std::cout << "0";
@@ -148,13 +170,13 @@ void LTC::decode(const TransitionSet& tSet) {
 
     } else if ((delta > 300) && (delta < 500)) {
       // start of 1
-      if (i + 1 >= tSet.size) {
-        std::cout << " missing last one i=" << i << " of " << (int)tSet.size
+      if (i + 1 >= tSet.size() ) {
+        std::cout << " missing last one i=" << i << " of " << (int)tSet.size()
                   << std::endl;
 
         return;
       }
-      uint32_t delta2 = tSet.t[i + 1] - tSet.t[i - 2];
+      uint32_t delta2 = tSet.delta( i+1 );
       if ((delta > 300) && (delta < 500)) {
         // found a 1
         std::cout << "1";
@@ -169,7 +191,7 @@ void LTC::decode(const TransitionSet& tSet) {
 
         i++;  // skip 2nd transtion on  main loop
       } else {
-        std::cout << " bad transition time i=" << i << " of " << (int)tSet.size
+        std::cout << " bad transition time i=" << i << " of " << (int)tSet.size()
                   << std::endl;
         return;
       }
@@ -237,19 +259,21 @@ void LTC::get(TimeCode& time) {
 int main(int argc, char* argv[]) {
   std::cout << "LTC Test" << std::endl;
 
-  TimeCode t1(7,0); // 11 * 60 + 22, 500000l);
-  std::cout << "t1=" << t1.disp() << std::endl;
-
+  TimeCode t1( 11 * 60 + 22, 500000l);
+  std::cout << "t1=" << t1.disp()  << " " <<  t1.microSeconds() <<  std::endl;
+  assert( t1.seconds() == 682 );
+  assert( t1.microSeconds() ==  500000l );
+  
   LTC ltc1(t1);
 
   TransitionSet tSet;
   ltc1.encode(tSet);
  std::cout << std::endl << "done encode" << std::endl;
 
-#if 0 
-  std::cout << "tSet size=" << (int)tSet.size << std::endl;
-  for( int i=1; i < tSet.size; i++ ){
-    std::cout << "t[" << i << "]=" << tSet.t[i] << "   " << tSet.t[i]- tSet.t[i-1] << std::endl;
+#if 0
+ std::cout << "tSet size=" << (int)tSet.size() << std::endl;
+ for( int i=1; i < tSet.size(); i++ ){
+   std::cout << "delta[" << i << "]=" << tSet.delta(i) << std::endl;
   }
 #endif
 
