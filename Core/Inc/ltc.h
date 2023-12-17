7b3ea271933b954492a0e133d0aa04d00bf83b61
Cullen Jennings <fluffy@iii.ca>
2023-12-17 10:46:39 +0800

    start moving C++ LTC code to C

---
+++ Core/Inc/ltc.h
@@ -0,0 +1,95 @@
+
+#if 0
+
+class LTC;
+
+class TimeCode {
+  friend class LTC;
+
+ public:
+  TimeCode() { valid = false; };
+  TimeCode(uint32_t s, uint32_t us) {
+    frame = (us * 30l / 1000000l) % 30;
+    sec = s % 60;
+    min = (s / 60) % 60;
+    hour = (s / 3600) % 24;
+    valid = true;
+  };
+
+  TimeCode(uint8_t h, uint8_t m, uint8_t s, uint8_t f)
+      : hour(h), min(m), sec(s), frame(f), valid(true) {
+    h = h % 24;
+    m = m % 60;
+    s = s % 60;
+  };
+
+  uint32_t seconds() const {
+    if (!valid) return 0;
+    return (uint32_t)sec + (uint32_t)min * 60l + (uint32_t)hour * 3600l;
+  };
+  uint32_t microSeconds() const {
+    if (!valid) return 0;
+    return (uint32_t)frame * 1000000l / 30l;
+  };
+
+  uint32_t disp() const {
+    if (!valid) {
+      return 0;
+    };
+    return frame + sec * 100l + min * 10000l + hour * 1000000l;
+  }
+
+  bool isValid() const { return valid; }
+
+ private:
+  uint8_t hour;
+  uint8_t min;
+  uint8_t sec;
+  uint8_t frame;
+  uint8_t valid;
+};
+
+class TransitionSet {
+  // circular buffer that holds 80 bits worth of transitions
+ public:
+  TransitionSet() : numTransitions(0){};
+  void add(uint32_t t) {
+    time[numTransitions % maxTransitions] = t;
+    numTransitions++;
+  };
+  uint32_t delta(uint16_t i) const {
+    if ((i < 1) || (i >= numTransitions)) return 0;
+    uint32_t curr = i % maxTransitions;
+    uint32_t prev = (i - 1) % maxTransitions;
+    return time[curr] - time[prev];
+  };
+  uint16_t size() const { return numTransitions; };
+  void clear() { numTransitions = 0; };
+
+ private:
+  static const uint16_t maxTransitions = 80 * 2 + 1;
+  uint32_t time[maxTransitions];
+  uint16_t numTransitions;
+};
+
+class LTC {
+ public:
+  LTC() { valid = 0; };
+  LTC(const TimeCode& time) {
+    set(time);
+    valid = 1;
+  };
+
+  void set(const TimeCode& time);
+  void get(TimeCode& time);
+
+  void encode(TransitionSet& tSet, uint8_t fps = 30);
+  void decode(const TransitionSet& tSet, uint8_t fps = 30);
+
+ private:
+  uint8_t bits[10];
+  uint8_t parity();
+  uint8_t valid;
+};
+
+#endif
--- Core/Src/ltc.c
+++ Core/Src/ltc.c
@@ -2,98 +2,10 @@
 //  Descrption of LTC can be found at https://en.wikipedia.org/wiki/Linear_timecode
 // Official specification in SMPTE 12M TODO
 
-#include <iostream>
 
-class LTC;
+#include "ltc.h"
 
-class TimeCode {
-  friend class LTC;
-
- public:
-  TimeCode() { valid = false; };
-  TimeCode(uint32_t s, uint32_t us) {
-    frame = (us * 30l / 1000000l) % 30;
-    sec = s % 60;
-    min = (s / 60) % 60;
-    hour = (s / 3600) % 24;
-    valid = true;
-  };
-
-  TimeCode(uint8_t h, uint8_t m, uint8_t s, uint8_t f)
-      : hour(h), min(m), sec(s), frame(f), valid(true) {
-    h = h % 24;
-    m = m % 60;
-    s = s % 60;
-  };
-
-  uint32_t seconds() const {
-    if (!valid) return 0;
-    return (uint32_t)sec + (uint32_t)min * 60l + (uint32_t)hour * 3600l;
-  };
-  uint32_t microSeconds() const {
-    if (!valid) return 0;
-    return (uint32_t)frame * 1000000l / 30l;
-  };
-
-  uint32_t disp() const {
-    if (!valid) {
-      return 0;
-    };
-    return frame + sec * 100l + min * 10000l + hour * 1000000l;
-  }
-
-  bool isValid() const { return valid; }
-
- private:
-  uint8_t hour;
-  uint8_t min;
-  uint8_t sec;
-  uint8_t frame;
-  uint8_t valid;
-};
-
-class TransitionSet {
-  // circular buffer that holds 80 bits worth of transitions
- public:
-  TransitionSet() : numTransitions(0){};
-  void add(uint32_t t) {
-    time[numTransitions % maxTransitions] = t;
-    numTransitions++;
-  };
-  uint32_t delta(uint16_t i) const {
-    if ((i < 1) || (i >= numTransitions)) return 0;
-    uint32_t curr = i % maxTransitions;
-    uint32_t prev = (i - 1) % maxTransitions;
-    return time[curr] - time[prev];
-  };
-  uint16_t size() const { return numTransitions; };
-  void clear() { numTransitions = 0; };
-
- private:
-  static const uint16_t maxTransitions = 80 * 2 + 1;
-  uint32_t time[maxTransitions];
-  uint16_t numTransitions;
-};
-
-class LTC {
- public:
-  LTC() { valid = 0; };
-  LTC(const TimeCode& time) {
-    set(time);
-    valid = 1;
-  };
-
-  void set(const TimeCode& time);
-  void get(TimeCode& time);
-
-  void encode(TransitionSet& tSet, uint8_t fps = 30);
-  void decode(const TransitionSet& tSet, uint8_t fps = 30);
-
- private:
-  uint8_t bits[10];
-  uint8_t parity();
-  uint8_t valid;
-};
+#if 0
 
 uint8_t LTC::parity() {
   uint8_t ret = 0;
@@ -280,6 +192,12 @@ void LTC::get(TimeCode& time) {
   time.valid = 1;
 }
 
+#endif 
+
+
+#if 0
+#include <iostream>
+
 int main(int argc, char* argv[]) {
   std::cout << "LTC Test" << std::endl;
 
@@ -296,12 +214,12 @@ int main(int argc, char* argv[]) {
   ltc1.encode(tSet);
   std::cout << "done encode" << std::endl;
 
-#if 0
+  if (0 ) {
  std::cout << "tSet size=" << (int)tSet.size() << std::endl;
  for( int i=1; i < tSet.size(); i++ ){
    std::cout << "delta[" << i << "]=" << tSet.delta(i) << std::endl;
   }
-#endif
+  }
 
   std::cout << "start decode" << std::endl;
 
@@ -317,3 +235,5 @@ int main(int argc, char* argv[]) {
 
   return 0;
 }
+
+#endif
