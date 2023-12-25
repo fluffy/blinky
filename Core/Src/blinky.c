c8860ac222afe1b13fdc3d0bcdb58bbf9d449e98
Cullen Jennings <fluffy@iii.ca>
2023-12-25 14:29:38 -0700

    start temperature sensor code

--- Core/Src/blinky.c
+++ Core/Src/blinky.c
@@ -567,6 +567,71 @@ void blinkSetup() {
     HAL_UART_Transmit(&hUartDebug, (uint8_t *)buffer, strlen(buffer), 1000);
   }
 
+  // access Temperature Sensor
+  if (1) {
+    char buffer[100];
+    const uint16_t i2cAddr = 0x48;
+    uint32_t timeout = 256;
+    uint8_t tempAddr = 0x0;
+    uint16_t temp=0;
+    uint8_t configAddr = 0x1;
+    uint16_t config=0;
+    HAL_StatusTypeDef status;
+    
+    status = HAL_I2C_IsDeviceReady(&hI2c, i2cAddr << 1, 2, timeout);
+    if (status != HAL_OK) {
+      snprintf(buffer, sizeof(buffer), "Error: Temperature sensor not found \r\n");
+      HAL_UART_Transmit(&hUartDebug, (uint8_t *)buffer, strlen(buffer), 1000);
+    }
+
+    status = HAL_I2C_Mem_Read(&hI2c, i2cAddr << 1, configAddr,
+                               sizeof(configAddr), (uint8_t *)&config,
+                               (uint16_t)sizeof(config), timeout);
+    if (status != HAL_OK) {
+      // stat: 0=0k, 1 is HAL_ERROR, 2=busy , 3 = timeout
+      snprintf(buffer, sizeof(buffer),
+               "Temperature Read config Error:  data hal error %d \r\n", status);
+      HAL_UART_Transmit(&hUartDebug, (uint8_t *)buffer, strlen(buffer), 1000);
+    }
+    else {
+      snprintf(buffer, sizeof(buffer),
+               "Temperature config :  0x%04x \r\n", config);
+      HAL_UART_Transmit(&hUartDebug, (uint8_t *)buffer, strlen(buffer), 1000);
+    }
+    
+    config = 0xA060; // power up default
+    config |= 0x1000; // turn of extended mode
+    uint16_t convRate =1; // 0=0.25Hz, 1=1Hz, 2=4Hz, 3=8Hz
+    config |= (config&0x3FFF) | ( convRate << 6 ); // set coversion rate 
+    
+    status = HAL_I2C_Mem_Write(&hI2c, i2cAddr << 1, configAddr,
+                               sizeof(configAddr), (uint8_t *)&config,
+                               (uint16_t)sizeof(config), timeout);
+    if (status != HAL_OK) {
+      // stat: 0=0k, 1 is HAL_ERROR, 2=busy , 3 = timeout
+      snprintf(buffer, sizeof(buffer),
+               "Temperature Write Error:  data hal error %d \r\n", status);
+      HAL_UART_Transmit(&hUartDebug, (uint8_t *)buffer, strlen(buffer), 1000);
+    }
+    
+    HAL_Delay( 1100 /*ms */); // TODO remove 
+    
+    status = HAL_I2C_Mem_Read(&hI2c, i2cAddr << 1, tempAddr,
+                              sizeof(tempAddr), (uint8_t *)&temp,
+                              (uint16_t)sizeof(temp), timeout);
+    if (status != HAL_OK) {
+      // stat: 0=0k, 1 is HAL_ERROR, 2=busy , 3 = timeout
+      snprintf(buffer, sizeof(buffer),
+               "Temperature Read Error:  data hal error %d \r\n", status);
+      HAL_UART_Transmit(&hUartDebug, (uint8_t *)buffer, strlen(buffer), 1000);
+    } else {
+       snprintf(buffer, sizeof(buffer),
+               "Temperature:  raw 0x%04x \r\n", temp);
+      HAL_UART_Transmit(&hUartDebug, (uint8_t *)buffer, strlen(buffer), 1000);
+    }
+    
+  }
+  
   // access EEProm
   if (1) {
     char buffer[100];
@@ -611,6 +676,8 @@ void blinkSetup() {
       // chip has 1.5 ms max page write time when it will not respond
       HAL_Delay(2 /*ms */);
       HAL_Delay(20 /*ms */);
+
+   
     }
 
     status = HAL_I2C_Mem_Read(&hI2c, i2cAddr << 1, eepromMemAddr,
