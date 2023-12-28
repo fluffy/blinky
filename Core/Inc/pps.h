1e17f50388a5d02529121e5db5e44237fa7708f7
Cullen Jennings <fluffy@iii.ca>
2023-12-28 13:03:14 -0700

    start refactor of PPS

---
+++ Core/Inc/pps.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 Cullen Jennings
+#pragma once
+
+void ppsInit();
+void ppsSetup();
+
+void ppsStart();
+void ppsStop();
+
---
+++ Core/Src/pps.c
@@ -0,0 +1,20 @@
+// Copyright (c) 2023 Cullen Jennings
+
+#include "pps.h"
+
+#include "hardware.h"
+
+
+void ppsInit(){
+}
+
+void ppsSetup(){
+}
+
+void ppsStart(){
+}
+
+void ppsStop(){
+}
+
+
