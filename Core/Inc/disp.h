bc6daae55f3b22687b3de450a6b907542d3f8207
Cullen Jennings <fluffy@iii.ca>
2024-01-15 15:24:59 -0700

    start seperation of disp code

---
+++ Core/Inc/disp.h
@@ -0,0 +1,7 @@
+// Copyright (c) 2023 Cullen Jennings
+#pragma once
+
+void dispInit();
+void dispSetup();
+
+void dispUpdate( int32_t dispUs, int32_t dispS ); // called in an interupt 
---
+++ Core/Src/disp.c
@@ -0,0 +1,22 @@
+// Copyright (c) 2023 Cullen Jennings
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stm32f4xx_ll_tim.h>
+#include <string.h>
+
+#include "hardware.h"
+#include "main.h"
+#include "disp.h"
+
+
+void dispInit(){
+}
+
+void dispSetup(){
+}
+
+void dispUpdate( int32_t dispUs, int32_t dispS ){
+   // called in an interupt
+}
