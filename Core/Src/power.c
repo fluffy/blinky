7241b2381d5bcf63b6878a2776d1e338190ec3ac
Cullen Jennings <fluffy@iii.ca>
2024-02-09 16:15:51 -0700

    correctly detect USB power available

--- Core/Src/power.c
+++ Core/Src/power.c
@@ -20,10 +20,14 @@ void powerSetup(){
   // TODO
 
   if ( config.product == 2) {
+    // only do for clock
+
+    uint16_t val1;
+    uint16_t val2;
     uint16_t val;
     int power;
 
-  // seeing value of 0 if USB connector is fliped wrong way
+  // value will be  0 if USB connector is fliped wrong way
   // value of 543 = on usb hub = 0.4375 V
   // value of 1138 , 1157 on mac front port = 0.916 V
   // value of 501 = on  usb expander bar
@@ -37,32 +41,18 @@ void powerSetup(){
   // < 2500 , have 3 A
 
 
-  // TODO need to check both channel 7 and 10 for two different sides of connector
+  // This code check both  both ADC channel 7 and 10 for two different sides of connector
 
   HAL_ADC_Start(&hadc1);
   HAL_ADC_PollForConversion(&hadc1, 1000 /*timeout ms*/);
-  val = HAL_ADC_GetValue(&hadc1);
-
-  power=0; // in mA
-  if ( ( val > 250) & ( val <= 750 ) ) {
-    power = 500;
-  }
-  if ( ( val > 750) & ( val <= 1500 ) ) {
-    power = 1500;
-  }
-  if ( ( val > 1500) & ( val <= 2500 ) ) {
-    power = 3000;
-  }
-  if (1) {
-    char buffer[100];
-    snprintf(buffer, sizeof(buffer), "  Power supply1: %d.%dA (value=%u)\r\n", power/1000, ( power/100) % 10 ,  val );
-    HAL_UART_Transmit(&hUartDebug, (uint8_t *)buffer, strlen(buffer), 1000);
-  }
-
+  val1 = HAL_ADC_GetValue(&hadc1);
 
   HAL_ADC_Start(&hadc2);
   HAL_ADC_PollForConversion(&hadc2, 1000 /*timeout ms*/);
-  val = HAL_ADC_GetValue(&hadc2);
+  val2 = HAL_ADC_GetValue(&hadc2);
+
+  val = (val1 > val2) ? val1 : val2;
+
 
   power=0; // in mA
   if ( ( val > 250) & ( val <= 750 ) ) {
@@ -76,11 +66,9 @@ void powerSetup(){
   }
   if (1) {
     char buffer[100];
-    snprintf(buffer, sizeof(buffer), "  Power supply2: %d.%dA (value=%u)\r\n", power/1000, ( power/100) % 10 ,  val );
+    snprintf(buffer, sizeof(buffer), "  Power supply: %d.%01dA (val1=%u val2=%u)\r\n", power/1000, ( power/100) % 10 ,  val1, val2 );
     HAL_UART_Transmit(&hUartDebug, (uint8_t *)buffer, strlen(buffer), 1000);
   }
-
-
 
   // TODO - if power, set status LED and error
   // TODO - if power good, enable 5V output
