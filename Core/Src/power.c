c0372f3fd635c4228c91f3c14b9051c27d903144
Cullen Jennings <fluffy@iii.ca>
2024-02-09 15:49:08 -0700

    fix but on adc1 for power detection

--- Core/Src/power.c
+++ Core/Src/power.c
@@ -20,11 +20,30 @@ void powerSetup(){
   // TODO
 
   if ( config.product == 2) {
+    uint16_t val;
+    int power;
+
+  // seeing value of 0 if USB connector is fliped wrong way
+  // value of 543 = on usb hub = 0.4375 V
+  // value of 1138 , 1157 on mac front port = 0.916 V
+  // value of 501 = on  usb expander bar
+
+  // the far end has 10k, 22K, 56K resisotr to 5V for 3, 1.5, 0.5 A respectively
+  // this end has 5.1K resitor to ground
+  // range of ADC is 4096 for 3.3 V
+  // < 250 , wrong side CC
+  // <  750 , have 0.5 A
+  // < 1500 , have 1.5 A
+  // < 2500 , have 3 A
+
+
   // TODO need to check both channel 7 and 10 for two different sides of connector
+
   HAL_ADC_Start(&hadc1);
   HAL_ADC_PollForConversion(&hadc1, 1000 /*timeout ms*/);
-  uint16_t val = HAL_ADC_GetValue(&hadc1);
-  int power=0; // in mA
+  val = HAL_ADC_GetValue(&hadc1);
+
+  power=0; // in mA
   if ( ( val > 250) & ( val <= 750 ) ) {
     power = 500;
   }
@@ -39,22 +58,12 @@ void powerSetup(){
     snprintf(buffer, sizeof(buffer), "  Power supply1: %d.%dA (value=%u)\r\n", power/1000, ( power/100) % 10 ,  val );
     HAL_UART_Transmit(&hUartDebug, (uint8_t *)buffer, strlen(buffer), 1000);
   }
-  // seeing value of 0 if USB connector is fliped wrong way
-  // value of 543 = on usb hub = 0.4375 V
-  // value of 1138 , 1157 on mac front port = 0.916 V
-  // value of 501 = on  usb expander bar
 
-  // the far end has 10k, 22K, 56K resisotr to 5V for 3, 1.5, 0.5 A respectively
-  // this end has 5.1K resitor to ground
-  // range of ADC is 4096 for 3.3 V
-  // < 250 , wrong side CC
-  // <  750 , have 0.5 A
-  // < 1500 , have 1.5 A
-  // < 2500 , have 3 A
 
-   HAL_ADC_Start(&hadc2);
+  HAL_ADC_Start(&hadc2);
   HAL_ADC_PollForConversion(&hadc2, 1000 /*timeout ms*/);
   val = HAL_ADC_GetValue(&hadc2);
+
   power=0; // in mA
   if ( ( val > 250) & ( val <= 750 ) ) {
     power = 500;
@@ -70,6 +79,8 @@ void powerSetup(){
     snprintf(buffer, sizeof(buffer), "  Power supply2: %d.%dA (value=%u)\r\n", power/1000, ( power/100) % 10 ,  val );
     HAL_UART_Transmit(&hUartDebug, (uint8_t *)buffer, strlen(buffer), 1000);
   }
+
+
 
   // TODO - if power, set status LED and error
   // TODO - if power good, enable 5V output
