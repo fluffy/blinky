2b5828b342638d430bb583c65df6c03aec1da2a9
Cullen Jennings <fluffy@iii.ca>
2024-02-10 11:06:01 -0700

    correct pin names

--- Core/Src/buttons.c
+++ Core/Src/buttons.c
@@ -24,6 +24,7 @@ void buttonsSetup() {
     return;
   }
 
+#if 1
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
 
@@ -42,6 +43,8 @@ void buttonsSetup() {
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init( BTN2_GPIO_Port, &GPIO_InitStruct);
+#endif
+
 }
 
 void buttonsRun() {
@@ -54,14 +57,16 @@ void buttonsRun() {
   uint32_t tick = HAL_GetTick();
 
   if (config.product == 1) {
-    if (!HAL_GPIO_ReadPin(AUX_CLK_GPIO_Port, GPS_PPS_Pin)) {
+    if (!HAL_GPIO_ReadPin( BTN2_GPIO_Port, BTN2_Pin)) {
       if (!button2WasPressed) {
+
         setting.blinkMute =
             (setting.blinkMute) ? 0 : 1;
 
         snprintf(buffer, sizeof(buffer), "Mute button pressed. Mute=%d \r\n",
                  (int)setting.blinkMute);
         HAL_UART_Transmit(&hUartDebug, (uint8_t *)buffer, strlen(buffer), 1000);
+
       }
       button2WasPressed = 1;
     } else {
@@ -69,8 +74,9 @@ void buttonsRun() {
     }
   }
 
-  if (HAL_GPIO_ReadPin(BOOT1_GPIO_Port, BOOT1_Pin)) {
+  if (HAL_GPIO_ReadPin(BTN3_GPIO_Port, BTN3_Pin)) {
     if (!button3WasPressed) {
+
       if (setting.blinkBlank) {
         setting.blinkDispAudio = 1;
         setting.blinkBlank = 0;
