262bd64d9419b414736b864d646b209ca36e88f7
Cullen Jennings <fluffy@iii.ca>
2024-02-13 13:31:53 -0700

    up version

--- Core/Src/blinky.c
+++ Core/Src/blinky.c
@@ -28,7 +28,7 @@
 // Uses Semantic versioning. See https://semver.org/
 // major.minor.patch,
 // patch=year/month/day
-const char *version = "0.102.240210";
+const char *version = "0.102.240213";
 
 
 Measurements data;
